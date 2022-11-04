b1c5a952cf2f72cfb99fe4182ca0bdf09d3f3b05
Lenovo <ramandeepsingh1511@gmail.com>
2022-11-04 15:06:27 +0530

    some code editing

---
+++ exp13/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Check String in Palidrome or not?</title>
+</head>
+
+<body>
+    <center>
+        <h1>Determine whether a string is palindrome or not? </h1>
+        <form action="" method="POST">
+            <label for="string">Enter a string: - </label>
+            <input type="text" name="string" placeholder="Enter a string">
+            <input type="submit" value="Check" name="submit">
+        </form>
+
+        <?php
+        // echo "raman";
+        if (isset($_POST['submit'])) {
+            $string = $_POST['string'];
+
+            function palindrome($string)
+            {
+                $revString = strrev($string);
+
+                // echo $revString;
+                if ($revString == $string) {
+                    echo $string . " is a Palindrome.\n";
+                } else {
+                    echo $string . " is not a Palindrome.\n";
+                }
+            }
+            palindrome($string);
+        }
+        ?>
+    </center>
+</body>
+
+</html>
