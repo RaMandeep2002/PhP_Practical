0e889e507b9ecf37dde4c9e7235a7ef484d8b7bf
Lenovo <ramandeepsingh1511@gmail.com>
2022-11-05 23:30:30 +0530

    some changes

--- exp10/index.php
+++ exp10/index.php
@@ -3,9 +3,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Program 10</title>
 </head>
 
@@ -34,7 +31,7 @@
             $make_marks = 400;
 
             $total = $num1 + $num2 + $num3 + $num4;
-            $average = $total / 3;
+            $average = $total / 4;
             echo "<br>Total marks of Four subjects = " . $total . "<br>";
             echo "Average marks of Four subjects = " . $average . "<br>";
 
